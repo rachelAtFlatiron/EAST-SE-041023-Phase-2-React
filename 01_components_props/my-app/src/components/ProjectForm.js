1bf8df4eef6aa7000fd0acc42d521af0526ed1a2
Rachel Katz <rachel.katz@flatironschool.com>
2023-05-01 14:31:44 -0400

    01 starter

---
+++ 01_components_props/my-app/src/components/ProjectForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ProjectForm() {
+    let arr = [1, 2, 3]
+  //jsx, ends up on the dom
+  return (
+    <div>
+        <div>ProjectForm</div>
+    </ div>
+  )
+}
+
+export default ProjectForm
