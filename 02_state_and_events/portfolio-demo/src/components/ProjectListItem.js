4059a96ed67e438c65f30a83ce4cb2df1e83b7a2
Rachel Katz <rachel.katz@flatironschool.com>
2023-05-02 11:36:55 -0400

    Revert "deliverables 1 + 2"
    This reverts commit ec00676f0bbe9c916d92490d05a30a81e19cac85.

--- 02_state_and_events/portfolio-demo/src/components/ProjectListItem.js
+++ 02_state_and_events/portfolio-demo/src/components/ProjectListItem.js
@@ -1,29 +1,12 @@
-//1. import useState
-import { useState } from "react";
+import React from "react";
 
 function ProjectListItem({ project }) {
-	//project.claps
 	let { name, about, image, claps, link, phase } = project;
-
-	//2. create state
-	// set to claps aka project.claps so that it is aligned with our data
-	const [projectClaps, setProjectClaps] = useState(claps)
-
-	//3. create callback function
-	const handleClap = () => {
-		//projectClaps = projectClaps + 1
-		//projectClaps += 1
-
-		//4. update state
-		setProjectClaps(prevProjectClaps => prevProjectClaps + 1)
-	}
-
 	return (
 		<li className="card">
 			<figure className="image">
 				<img src={image} alt={name} />
-				{ /* 5. create event handler */ }
-				<button onClick={handleClap} className="claps">👏{projectClaps}</button>
+				<button className="claps">👏{claps}</button>
 			</figure>
 			<section className="details">
 				<h4>{name}</h4>
