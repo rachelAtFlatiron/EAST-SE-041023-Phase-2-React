ec00676f0bbe9c916d92490d05a30a81e19cac85
Rachel Katz <rachel.katz@flatironschool.com>
2023-05-02 11:31:53 -0400

    deliverables 1 + 2

--- 02_state_and_events/portfolio-demo/src/App.js
+++ 02_state_and_events/portfolio-demo/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Header from "./components/Header";
 import ProjectList from "./components/ProjectList"
 import ProjectForm from "./components/ProjectForm"
@@ -5,14 +6,38 @@ import projects from './projects'
 
 
 function App() {
+
+  //initialize darkMode state
+  const [darkMode, setDarkMode] = useState(true)
+
+  //call this whatever you want
+  //may be passed down as prop (see later lectures)
+  const handleClick = () => {
+    //if you are basing new state off of previous state
+    setDarkMode(prevDarkMode => !prevDarkMode)
+
+    //if new state is NOT based off previous state
+    //just pass in the new value
+    //setDarkMode("skdfj")
+  }
+
+  //on<eventname>
+  //there will be an onEvent for every native DOM event
   return (
-    <div className="App">
+    <div className={darkMode ? "App" : "App light"}>
       <Header />
-      <button>Dark Mode</button>
+      <button onClick={handleClick}>{darkMode ? "Light Mode" : "Dark Mode"}</button>
       <ProjectForm />
       <ProjectList projects = {projects}/>
     </div>
   );
 }
+
+// create state -> 
+// loads JSX / view (dark mode) -> 
+// person fires onClick ->
+// state gets updated ->
+// component re-renders/updates ->
+// light mode
 
 export default App;
--- 02_state_and_events/portfolio-demo/src/components/ProjectListItem.js
+++ 02_state_and_events/portfolio-demo/src/components/ProjectListItem.js
@@ -1,12 +1,29 @@
-import React from "react";
+//1. import useState
+import { useState } from "react";
 
 function ProjectListItem({ project }) {
+	//project.claps
 	let { name, about, image, claps, link, phase } = project;
+
+	//2. create state
+	// set to claps aka project.claps so that it is aligned with our data
+	const [projectClaps, setProjectClaps] = useState(claps)
+
+	//3. create callback function
+	const handleClap = () => {
+		//projectClaps = projectClaps + 1
+		//projectClaps += 1
+
+		//4. update state
+		setProjectClaps(prevProjectClaps => prevProjectClaps + 1)
+	}
+
 	return (
 		<li className="card">
 			<figure className="image">
 				<img src={image} alt={name} />
-				<button className="claps">👏{claps}</button>
+				{ /* 5. create event handler */ }
+				<button onClick={handleClap} className="claps">👏{projectClaps}</button>
 			</figure>
 			<section className="details">
 				<h4>{name}</h4>
