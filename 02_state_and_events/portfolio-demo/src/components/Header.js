9339f93e33be199a9aa3c989efda75c43fc0a8e7
Rachel Katz <rachel.katz@flatironschool.com>
2023-05-02 14:46:15 -0400

    deliverable #3

--- 02_state_and_events/portfolio-demo/src/components/Header.js
+++ 02_state_and_events/portfolio-demo/src/components/Header.js
@@ -3,11 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <header>
-<<<<<<< HEAD
-        <h1><span className='logo'>{'//'}</span>Portfolio Showcase</h1>
-=======
         <h1><span className="logo">{'//'}</span>Portfolio Showcase</h1>
->>>>>>> 02_starter
     </header>
   )
 }
--- 02_state_and_events/portfolio-demo/src/components/ProjectList.js
+++ 02_state_and_events/portfolio-demo/src/components/ProjectList.js
@@ -1,50 +1,50 @@
 import { useState } from 'react'
 import ProjectListItem from "./ProjectListItem";
 
+/*
+
+1. import useState
+2. initialized state to 0 (represent all projects)
+3. written updatePhase which will setPhase based on argument
+4. passed in updatePhase as the callback function for onClick
+5. created a filteredProjects array to contain only projects that match phaseState
+
+*/
+
 function ProjectList({ projects }) {
 
-	//ask what would be a good starting value
-	const [phase, setPhase] = useState(0)
+	//phase represents number to match projects.phase === phase
+	//initial state is 0, 0 will represent all projects
+	const [phaseState, setPhase] = useState(0)
 
-	const handleClick = (num) => {
-		setPhase(num)
+
+	//recreated every time component re-renders
+	const filteredProjects = projects.filter(project => 
+		//one line of code, so don't need curly braces and explicit return statement
+		phaseState === 0 || project.phase === phaseState
+	)
+
+	//callback function to be used in onClick events
+	// const updatePhase = (phaseNumber) => {
+	// 	setPhase(phaseNumber)
+	// }
+	function updatePhase(phaseNumber) {
+		setPhase(phaseNumber)
 	}
-
-	//every time phase gets updated
-	//code of ProjectList will re-run
-	//thus every time phase gets updated filteredProjects will update
-	//without needing to put filteredProjects in state
-	const filteredProjects = projects.filter(el => {
-		return (el.phase === phase || phase === 0)
-	})
 
 	return (
 		<section>
 			<h2>Project List</h2>
 			<div className="filter">
-<<<<<<< HEAD
-				<button onClick={() => handleClick(0)}>All</button>
-				<button onClick={() => handleClick(5)}>Phase 5</button>
-				<button onClick={() => handleClick(4)}>Phase 4</button>
-				<button onClick={() => handleClick(3)}>Phase 3</button>
-				<button onClick={() => handleClick(2)}>Phase 2</button>
-				<button onClick={() => handleClick(1)}>Phase 1</button>
-				{/* can't use for loop, JSX always expects an object to be displayed */}
-				{/* {
-					[1, 2, 3, 4, 5].map(el => {
-						return <button onClick={() => handleClick(el)}>Phase {el}</button>
-					})
-				} */}
-=======
-				<button>All</button>
-				<button>Phase 5</button>
-				<button>Phase 4</button>
-				<button>Phase 3</button>
-				<button>Phase 2</button>
-				<button>Phase 1</button>
->>>>>>> 02_starter
+				<button onClick={() => updatePhase(0)}>All</button>
+				<button onClick={() => updatePhase(5)}>Phase 5</button>
+				<button onClick={() => updatePhase(4)}>Phase 4</button>
+				<button onClick={() => updatePhase(3)}>Phase 3</button>
+				<button onClick={() => updatePhase(2)}>Phase 2</button>
+				<button onClick={() => updatePhase(1)}>Phase 1</button>
 			</div>
 			<ul className="cards">
+				{/* instead of ALL projects, we only want filtered projects */}
 				{filteredProjects.map((project) => (
 					<ProjectListItem key={project.id} project={project} />
 				))}
