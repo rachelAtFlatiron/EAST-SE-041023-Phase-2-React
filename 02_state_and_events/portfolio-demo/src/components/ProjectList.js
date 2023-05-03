a8b23a56cf832bd4f6066fb7056b4d33baacdea1
Rachel Katz <rachel.katz@flatironschool.com>
2023-05-03 13:35:29 -0400

    included phase button setPhase as callback

--- 02_state_and_events/portfolio-demo/src/components/ProjectList.js
+++ 02_state_and_events/portfolio-demo/src/components/ProjectList.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from "react";
 import ProjectListItem from "./ProjectListItem";
 
 /*
@@ -12,36 +12,42 @@ import ProjectListItem from "./ProjectListItem";
 */
 
 function ProjectList({ projects }) {
-
 	//phase represents number to match projects.phase === phase
 	//initial state is 0, 0 will represent all projects
-	const [phaseState, setPhase] = useState(0)
-
+	const [phaseState, setPhase] = useState(0);
 
 	//recreated every time component re-renders
-	const filteredProjects = projects.filter(project => 
-		//one line of code, so don't need curly braces and explicit return statement
-		phaseState === 0 || project.phase === phaseState
-	)
+	const filteredProjects = projects.filter(
+		(project) =>
+			//one line of code, so don't need curly braces and explicit return statement
+			phaseState === 0 || project.phase === phaseState
+	);
 
 	//callback function to be used in onClick events
 	// const updatePhase = (phaseNumber) => {
 	// 	setPhase(phaseNumber)
 	// }
 	function updatePhase(phaseNumber) {
-		setPhase(phaseNumber)
+		setPhase(phaseNumber);
 	}
 
 	return (
 		<section>
 			<h2>Project List</h2>
 			<div className="filter">
-				<button onClick={() => updatePhase(0)}>All</button>
+				{/* <button onClick={() => updatePhase(0)}>All</button>
 				<button onClick={() => updatePhase(5)}>Phase 5</button>
 				<button onClick={() => updatePhase(4)}>Phase 4</button>
 				<button onClick={() => updatePhase(3)}>Phase 3</button>
 				<button onClick={() => updatePhase(2)}>Phase 2</button>
-				<button onClick={() => updatePhase(1)}>Phase 1</button>
+				<button onClick={() => updatePhase(1)}>Phase 1</button> */}
+
+				<button onClick={() => setPhase(0)}>All</button>
+				<button onClick={() => setPhase(5)}>Phase 5</button>
+				<button onClick={() => setPhase(4)}>Phase 4</button>
+				<button onClick={() => setPhase(3)}>Phase 3</button>
+				<button onClick={() => setPhase(2)}>Phase 2</button>
+				<button onClick={() => setPhase(1)}>Phase 1</button>
 			</div>
 			<ul className="cards">
 				{/* instead of ALL projects, we only want filtered projects */}
