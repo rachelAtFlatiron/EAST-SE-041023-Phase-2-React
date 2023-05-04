7bcfb38d3ac9f37578cb160c7dd0c24a7cfc5f9b
Rachel Katz <rachel.katz@flatironschool.com>
2023-05-04 17:15:06 -0400

    removed updateState

--- 05_sidefx_fetching/portfolio-demo/src/components/ProjectList.js
+++ 05_sidefx_fetching/portfolio-demo/src/components/ProjectList.js
@@ -15,21 +15,16 @@ function ProjectList({ projects }) {
 		}
 	);
 
-	function updatePhase(phaseNumber) {
-		setPhase(phaseNumber)
-	}
-
-
 	return (
 		<section>
 			<h2>Project List</h2>
 			<div className="filter">
-				<button onClick={() => updatePhase(0)}>All</button>
-				<button onClick={() => updatePhase(5)}>Phase 5</button>
-				<button onClick={() => updatePhase(4)}>Phase 4</button>
-				<button onClick={() => updatePhase(3)}>Phase 3</button>
-				<button onClick={() => updatePhase(2)}>Phase 2</button>
-				<button onClick={() => updatePhase(1)}>Phase 1</button>
+				<button onClick={() => setPhase(0)}>All</button>
+				<button onClick={() => setPhase(5)}>Phase 5</button>
+				<button onClick={() => setPhase(4)}>Phase 4</button>
+				<button onClick={() => setPhase(3)}>Phase 3</button>
+				<button onClick={() => setPhase(2)}>Phase 2</button>
+				<button onClick={() => setPhase(1)}>Phase 1</button>
 			</div>
 			<input
 				type="text"
